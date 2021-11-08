91af5df23653014a9229043767c4aa65632a946e
blue333 <1014464483@qq.com>
2021-11-08 23:04:53 +0800

    ...

--- 第三次作业/lv0.js
+++ 第三次作业/lv0.js
@@ -2,13 +2,11 @@ let obj = new Object();
 obj.name = 'only';
 obj.age = 100;
 console.log(obj);
-// 原来不能创两个obj，那我怎么做作业（泣）
 let obj1 = {
     name: 'only',
     age: 21
 }
 console.log(obj1);
-// 竟然换个名字就好了吗
 /*使用构造函数创建对象*/
 function pig(){
     this.name = 'peppa pig';
